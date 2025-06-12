e4bdbd5ec7ec27348a6ecb08da04157c5d95eca9
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-06-12 12:41:44 -0300

    feat: arq basico1

---
+++ basico1.py
@@ -0,0 +1,10 @@
+print(12, 34, 1011, sep="-", end="###")
+
+inteiro = 10
+print(inteiro)
+inteiro += 10
+print(inteiro)
+
+floatzin = float(inteiro)
+print(floatzin)
+#funciona com todos bool() str() int() float()  etc
