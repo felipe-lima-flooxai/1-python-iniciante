02c14b743f46dcc7afe8673760612c7ef3fd0b8a
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-06-13 11:08:23 -0300

    feat: basico3 or

--- basico3.py
+++ basico3.py
@@ -4,3 +4,8 @@ idade = input('Qual sua idade ')
 texto = f"Seu nome é {nome} e sua idade é {idade}"
 
 print(texto)
+
+senha = input("Senha: ") or "Sem senha"
+print("senha ", senha)
+
+#funciona tb tipo false or 0 or "Joaquim" or "Jamal" => imprime joaquim, pq é o primeiro valor true
