5aeb8e8d5516bab2ad803c70a2065128b0517c08
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-06-17 12:14:49 -0300

    feat: basico13 final

---
+++ basico13.py
@@ -0,0 +1,10 @@
+def printando(frase, vezes):
+    contador = 0
+    while contador < vezes:
+        print(frase)
+        contador += 1
+
+valor = "Hoje é um bom dia"
+vezes = 5
+
+printando(valor, vezes)
